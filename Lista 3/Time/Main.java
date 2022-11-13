64d71370eee15972257cb72a4490429b8ae5f1b9
Leonam Rabelo <53281220+LeonamRabelo@users.noreply.github.com>
2022-11-13 01:31:07 -0300

    Add files via upload

---
+++ Lista 3/Time/Main.java
@@ -0,0 +1,8 @@
+public class Main{
+    public static void main(String[] args) {
+        System.out.println("Hora agora");
+        Time time1 = new Time();
+        time1.setTime(12, 22, 12);
+        time1.imprimeTime();
+    }
+}
