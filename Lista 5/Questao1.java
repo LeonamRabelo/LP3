b560ab9c2ff231e16dca7f563c60f33399083302
Leonam Rabelo <53281220+LeonamRabelo@users.noreply.github.com>
2022-11-27 20:10:54 -0300

    Add files via upload

---
+++ Lista 5/Questao1.java
@@ -0,0 +1,9 @@
+import ant.Contato;
+
+public class Questao1{
+    public static void main(String[]args){
+        Contato cont1 = new Contato("Lion R", "lion12@gmailcom", "73988599735", 26, 9, 2002);
+
+        System.out.println(cont1.getContador());
+    }
+}
---
+++ Lista 5/Questao2.java
@@ -0,0 +1,9 @@
+import ant.Time;
+
+public class Questao2{
+    public static void main(String[] args){
+        Time t = new Time(15, 19, 28);
+
+        System.out.println(Time.diffTime(t));
+    }
+}
