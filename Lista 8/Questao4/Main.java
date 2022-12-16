2064214a9c66fb11c5742f0f697e57eb657a9531
Leonam Rabelo <53281220+LeonamRabelo@users.noreply.github.com>
2022-12-15 23:56:27 -0300

    Add files via upload

---
+++ Lista 8/Questao4/Main.java
@@ -0,0 +1,6 @@
+public class Main{
+    public static void main(String[]args){
+        //PessoaFisica pessoaFisica = new PessoaFisica("lion","Ilheus, 50", "8282812818",
+        //  "lion@gm.com", 22, 9, 2002, "231883218321" );
+    }
+}
